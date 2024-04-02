f2e983cc2137439bbe2f133fc9972a6420c5102d
DESKTOP-38924LQ\user1 <taewookim02@gmail.com>
2024-04-02 17:08:40 +0900

    Add login and memberVo

--- jdbcWorkspace/src/member/MemberController.java
+++ jdbcWorkspace/src/member/MemberController.java
@@ -2,12 +2,12 @@ package member;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import main.Main;
 import util.JDBCTemplate;
 
 public class MemberController {
-	
 
 	public void printMenu() {
 		System.out.println("====MEMBER====");
@@ -28,38 +28,68 @@ public class MemberController {
 		}
 	}
 
-
 	private void login() {
-		String sql = "SELECT * FROM member WHERE ID = ? AND PWD = ?";
+		try {
+			Connection conn = JDBCTemplate.getConn();
+			String sql = "SELECT * FROM MEMBER WHERE ID = ? AND PWD = ?";
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+
+			System.out.print("아이디: ");
+			String inputId = Main.SC.nextLine();
+			System.out.print("비밀번호: ");
+			String inputPwd = Main.SC.nextLine();
+			pstmt.setString(1, inputId);
+			pstmt.setString(2, inputPwd);
+			
+			ResultSet rs = pstmt.executeQuery();
+			if (!rs.next()) {
+				System.out.println("로그인 실패.");
+				return;
+			}
+			
+			String no = rs.getString("NO");
+			String id = rs.getString("ID");
+			String pwd = rs.getString("PWD");
+			String nick = rs.getString("NICK");
+			String joindate = rs.getString("JOIN_DATE");
+			String modifyDate = rs.getString("MODIFY_DATE");
+			String quitYn = rs.getString("QUIT_YN");
+			String adminYn = rs.getString("ADMIN_YN");
+			MemberVo vo = new MemberVo(no, id, pwd, nick, joindate, modifyDate, quitYn, adminYn);
+			System.out.println(vo);
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
-	
-	
+
 	private void join() {
 		try {
 			Connection conn = JDBCTemplate.getConn();
 			String sql = "INSERT INTO MEMBER(NO, ID, PWD, NICK) VALUES(SEQ_MEMBER_NO.NEXTVAL, ?, ?, ?)";
-			
+
 			PreparedStatement pstmt = conn.prepareStatement(sql);
-			
-			System.out.print("id: ");
+
+			System.out.print("아이디: ");
 			String inputId = Main.SC.nextLine();
-			System.out.print("pwd: ");
+			System.out.print("비밀번호: ");
 			String inputPwd = Main.SC.nextLine();
-			System.out.print("nick: ");
+			System.out.print("닉네임: ");
 			String inputNick = Main.SC.nextLine();
 			pstmt.setString(1, inputId);
 			pstmt.setString(2, inputPwd);
 			pstmt.setString(3, inputNick);
-			
+
 			int r = pstmt.executeUpdate();
-			
+
 			if (r != 1) {
 				System.out.println("회원가입 오류.");
 				return;
 			}
 			System.out.println("회원가입 성공!");
-			
+
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();

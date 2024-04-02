d6e4e5adaa7325c82b533ad3b9de34dc09ad5261
DESKTOP-38924LQ\user1 <taewookim02@gmail.com>
2024-04-02 17:27:25 +0900

    Add MemberVo import

--- jdbcWorkspace/src/main/Main.java
+++ jdbcWorkspace/src/main/Main.java
@@ -3,10 +3,12 @@ package main;
 import java.util.Scanner;
 
 import member.MemberController;
+import member.MemberVo;
 
 public class Main {
 	public static final Scanner SC = new Scanner(System.in);
 	public static MemberVo loginMember = null;
+
 	public static void main(String[] args) {
 		MemberController mc = new MemberController();
 

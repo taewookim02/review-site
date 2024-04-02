fb5a1e7ccb2ab096bd5b3248c34a152515922956
DESKTOP-38924LQ\user1 <taewookim02@gmail.com>
2024-04-02 17:16:42 +0900

    Add default menu

--- jdbcWorkspace/src/main/Main.java
+++ jdbcWorkspace/src/main/Main.java
@@ -4,9 +4,35 @@ import java.util.Scanner;
 
 public class Main {
 	public static final Scanner SC = new Scanner(System.in);
-	
+
 	public static void main(String[] args) {
-		System.out.println("hello");
+		MemberController mc = new MemberController();
+
+		System.out.println("====MENU====");
+		while (true) {
+			System.out.println("1. MEMBER");
+			System.out.println("2. ...");
+			System.out.println("9. 종료하기");
+
+			System.out.print("메뉴번호: ");
+			String menu = Main.SC.nextLine();
+
+			switch (menu) {
+			case "1":
+				mc.printMenu();
+				break;
+			case "2":
+				System.out.println("구현중..");
+				break;
+			case "9":
+				System.out.println("프로그램 종료");
+				return;
+
+			default:
+				System.out.println("잘못 입력하셨습니다.");
+				break;
+			}
+		}
 	}
 
 }

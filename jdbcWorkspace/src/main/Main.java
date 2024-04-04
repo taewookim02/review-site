f4f45e482127a2545e98aae247260d5eff7b1a01
DESKTOP-7MHC037\user1 <geuna_1019@naver.com>
2024-04-04 12:25:19 +0900

    머지 충돌 해결

--- jdbcWorkspace/src/main/Main.java
+++ jdbcWorkspace/src/main/Main.java
@@ -5,18 +5,22 @@ import java.util.Scanner;
 import foodprod.FoodProd;
 import member.MemberController;
 import member.MemberVo;
+import normalProd.NormalProdController;
 
 public class Main {
 	public static final Scanner SC = new Scanner(System.in);
 	public static MemberVo loginMember = null;
 
 	public static void main(String[] args) throws Exception {
-		MemberController mc = new MemberController();
 		FoodProd fp = new FoodProd();
+		MemberController mc = new MemberController();
+		NormalProdController nc = new NormalProdController();
+
 		System.out.println("====MENU====");
 		while (true) {
 			System.out.println("1. MEMBER");
 			System.out.println("2. food");
+			System.out.println("3. normal");
 			System.out.println("9. 종료하기");
 
 			System.out.print("메뉴번호: ");
@@ -28,6 +32,9 @@ public class Main {
 				break;
 			case "2":
 				fp.printMenu();
+				break;
+			case "3":
+				nc.printMenu();
 				break;
 			case "9":
 				System.out.println("프로그램 종료");
